f1f0a883b37f60792876f9042fd95be3f4f12e43
Peter Mallett <pmallett@redhat.com>
2025-07-18 14:00:18 -0600

    add initial page structure and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MastheadComponent } from "./masthead/masthead.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MastheadComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'angular-patternfly';
-}
+export class AppComponent {}
